6480f0b4107c111e9a5c60423a2bdecaa21014e1
tennosys <tennosys@gmail.com>
2018-02-19 14:20:26 +0700

    remove unused, change svg

--- src/js/lib/router.js
+++ src/js/lib/router.js
@@ -3,14 +3,6 @@ export const installRouter = (locationUpdatedCallback) => {
     if (e.defaultPrevented || e.button !== 0 ||
         e.metaKey || e.ctrlKey || e.shiftKey) return;
 
-    const anchor = e.composedPath().filter(n => n.tagName === 'A')[0];
-    if (!anchor || anchor.target ||
-        anchor.hasAttribute('download') ||
-        anchor.getAttribute('rel') === 'external') return;
-
-    const href = anchor.href;
-    if (!href || href.indexOf('mailto:') !== -1) return;
-
     const location = window.location;
     const origin = location.origin || location.protocol + '//' + location.host;
     if (href.indexOf(origin) !== 0) return;
